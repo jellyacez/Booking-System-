8bd0e4065df74322ba3291d4fdf03b3873c64401
Astaire <67066087+jellyacez@users.noreply.github.com>
2025-10-25 17:42:22 +0800

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="/css/login.css">
     
     <title>Document</title>
 </head>
@@ -63,3 +63,4 @@ if (isset($_POST['login'])) {
 
 
 ?>
+
